fe4e41f988b2ca19f13f235331fc161f146d503b
Vadym <vadym.ponomarov@estcube.eu>
2017-11-13 23:10:44 +0100

    Menu bundle + bootstrap

---
+++ src/AppBundle/Menu/Builder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Menu;
+
+// Third party bundles
+use Knp\Menu\MenuFactory;
+
+class Builder
+{
+  public function mainMenu(MenuFactory $factory, array $options)
+  {
+    // Menu bundle (external)
+    $menu = $factory->createItem('root');
+    $menu->addChild('Home', ['route' => 'homepage']);
+
+    // Add bootstrap class for menu items
+    $menu->setChildrenAttribute('class', 'nav navnar-nav');
+    return $menu;
+  }
+}
