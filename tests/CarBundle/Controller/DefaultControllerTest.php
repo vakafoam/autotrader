782b08454e9f2b7a53c92cb7a456af1aee772023
Vadym <vadym.ponomarov@estcube.eu>
2018-01-23 13:45:13 +0200

    First unit test

--- tests/CarBundle/Controller/DefaultControllerTest.php
+++ tests/CarBundle/Controller/DefaultControllerTest.php
@@ -12,6 +12,6 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertContains('Hello World', $client->getResponse()->getContent());
+        $this->assertContains('Welcome to Autotrader',  $crawler->filter('h1')->text());
     }
 }
